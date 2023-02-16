3a5609f6c0764fd53db8631da101104abf71987e
OKydalov <123094881+OKydalov@users.noreply.github.com>
2023-02-16 15:07:24 +0100

    Kydalov hw8

---
+++ Circle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_8_2023
+{
+    class Circle : Shapes
+    {
+        private int radius;
+        private string size;
+        public int Radius { get { return radius; } }
+        public string Size { get { return size; } }
+
+        public Circle(string name, int radius, string size) : base(name)
+        {
+            this.size = size;
+            this.radius = radius;
+        }
+        public override double Area()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+
+        public override double Perimeter()
+        {
+            return 2 * Radius * Math.PI; 
+        }
+        public override void Print()
+        {
+            Console.WriteLine("Circle method Print:\n{0} have Area : {1:###.##} and Perimeter :{2:###.##} Size {3}", Name, Area(), Perimeter(), size);
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HW_8_2023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double maxPerimetr = 0.0;
+            List<Shapes> shapes = new();
+            shapes.Add(new Square("square1", 8, "Big"));
+            shapes.Add(new Square("square2", 3, "Small"));
+            shapes.Add(new Square("square3", 6, "Medium"));
+            shapes.Add(new Circle("circle1", 2, "Small"));
+            shapes.Add(new Circle("circle2", 5, "Medium"));
+            shapes.Add(new Circle("circle3", 12, "Big"));
+            foreach (var item in shapes)
+            {
+                item.Print();
+            }
+            Console.WriteLine("Enter name of shape to find it : ");
+            string input = Console.ReadLine();
+            foreach (var item in shapes)
+            {
+                if (item.Name == input)
+                {
+                    item.Print();
+                }
+            }
+            foreach (Shapes s in shapes)
+            {
+                if (maxPerimetr <= s.Perimeter())
+                {
+                    maxPerimetr = s.Perimeter();
+                }
+            }
+            foreach (Shapes o in shapes)
+            {
+                if (maxPerimetr == o.Perimeter())
+                {
+                    Console.WriteLine($"Name: {o.Name}, with max perimetr:{o.Perimeter():###.##} ");
+                }
+            }
+            shapes.Sort();
+            foreach (var item in shapes)
+            {
+                Console.WriteLine($"Shape : {item} with perimeter : {item.Perimeter():###.##}");
+            }
+        }
+    }
+}
